8310465c015b886cca11d426f6839a39f8789edd
Daniel Pieper <daniel_pieper1991@hotmail.com>
2023-02-13 10:51:51 +0000

    first javascript lesson

---
+++ week_6/day_1/hello_world.js
@@ -0,0 +1,32 @@
+let myPet = "Cat";
+
+// console.log("My pet is:", myPet);
+
+const daysInYear = 365;
+
+const monthsInYear = 12;
+
+const averageDaysInMonth = daysInYear/monthsInYear;
+
+// console.log(Math.round(averageDaysInMonth));
+
+const randomNumber = Math.floor(Math.random()*10) + 1;
+
+// console.log(randomNumber);
+
+const favouriteFilms = ["Star Wars", "Lord of the Rings", "Gladiator"];
+
+favouriteFilms.push("Jurassic Park");
+
+// console.log(favouriteFilms);
+
+const dog = {
+    name: "Oscar",
+    breed: "Shih Tzu",
+    favFoods: ["Cheese", "Chum", "Sausages"]
+};
+
+// console.log(dog.favFoods)
+
+
+
