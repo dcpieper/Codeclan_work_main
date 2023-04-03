c8894a17ca8d44a26c049fb7fe597b83e3a815ad
Daniel Pieper <daniel_pieper1991@hotmail.com>
2023-04-03 12:02:13 +0100

    weekend homework

---
+++ week_12/day_3/inheritance_start_point/src/main/java/Instructor.java
@@ -0,0 +1,15 @@
+public class Instructor extends Person {
+
+    private String moduleTeam;
+    public Instructor(String name, String cohort, String moduleTeam) {
+        super(name, cohort);
+    }
+
+    public String getModuleTeam() {
+        return moduleTeam;
+    }
+
+    public void setModuleTeam(String moduleTeam) {
+        this.moduleTeam = moduleTeam;
+    }
+}
